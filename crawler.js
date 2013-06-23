09e89faa412691f0328a233be301a2d2b58b09a2
Jan Oevermann <jan@nurzubesuch.net>
2013-06-23 16:07:52 +0200

    initial commit

---
+++ crawler.js
@@ -0,0 +1,54 @@
+// copy/paste into web developer console on any wikipedia article
+
+var nodes = {};
+var nodeNr = 0;
+
+// get root node from page heading
+var rootNode = $("#firstHeading span").text();
+
+// set ajax requests to sync
+$.ajaxSetup({async:false});
+ 
+// get all nodes of article
+var extractNodes = function(name){        
+  nodes[name] = [];
+
+  // loop over given array
+  for(var i=0;i<name.length; i++){
+    
+    console.log(name[i]);
+    
+    if(name[i]){
+      // replace content on page with new content from target node
+      $("#mw-content-text").load(name[i].split(' ').join('_')+" #mw-content-text");
+    }else{
+      // job finished if there are no more nodes
+      console.log("finished");
+      console.log(Object.keys(nodes).length + " nodes on first level");
+      return;
+    }
+    
+    // extract all links from new content
+    $("#mw-content-text a[href^='/wiki']").each(function(){
+      title = $(this).attr("title");
+      link = $(this).attr("href");
+
+      // filter out internal wikipedia links
+      if (title && link.indexOf(":") === -1) {
+        nodes[name].push(title);
+      }
+    }); 
+  };
+
+  nodeNr++;
+  var label = nodes[rootNode][nodeNr];
+  extractNodes([label]);
+}
+
+// Start
+extractNodes([rootNode]);
+
+// Show results as JSON-String
+  $("#firstHeading span").text("Result");
+  nodeData = JSON.stringify(nodes);
+  $("#mw-content-text").html(nodeData);
